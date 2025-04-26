4a4827fc173fd6e11b152538a320c36eb83c43b2
ErsinAgirman <117252660+ErsinAgirman@users.noreply.github.com>
2025-04-26 23:57:21 +0300

    Sprites

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -4,7 +4,8 @@ public class CameraFollow : MonoBehaviour
 {
     [Header("Follow Settings")]
     [SerializeField] private Transform target;
-    [SerializeField] private Vector3 offset = new Vector3(0f, 0f, -10f);
+    [SerializeField] private Vector3 offset = new Vector3(0f, 15f, -15f);
+
     [SerializeField] private float followSpeed = 3f;  // ne kadar büyükse daha hızlı “takip”
 
     private void LateUpdate()
