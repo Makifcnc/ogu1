1e5625ac0b57356b40606443e14ba9aa3a260363
ErsinAgirman <117252660+ErsinAgirman@users.noreply.github.com>
2025-04-26 20:31:51 +0300

    NPC added

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -2,15 +2,89 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private DialogueSO dialogueSO;
+    [SerializeField] private GameObject infoPanel;
+    [SerializeField] private short facingRight = 1;
+    [SerializeField] private DialogueSO actionableDialog;
+
+    private Animator anim;
+    private bool isAnimating = false;
+    private bool isPlayerNear = false;
+
+    void Awake()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+        DialogueManager.DialogueEnterEvent += DialogueEnter;
+        DialogueManager.DialogueExitEvent += DialogueExit;
+    }
+
+    void OnDisable()
+    {
+        DialogueManager.DialogueEnterEvent -= DialogueEnter;
+        DialogueManager.DialogueExitEvent -= DialogueExit;
+    }
+
     void Update()
     {
-        
+        if (isPlayerNear && Input.GetKeyDown(KeyCode.E))
+        {
+            Interact();
+        }
+    }
+
+    private void DialogueEnter(DialogueSO asd)
+    {
+        if (isAnimating || actionableDialog == null) return;
+
+        if (actionableDialog == asd)
+        {
+            isAnimating = true;
+            anim.SetTrigger("hat");
+            //AudioManager.Instance.PlayMusic("music1");
+        }
+    }
+
+    private void DialogueExit(DialogueSO asd)
+    {
+        // Dialog bitince yapılacaklar (şimdilik boş bırakabiliriz)
+    }
+
+    public void Interact()
+    {
+        DialogueManager.Instance.StartDialogue(dialogueSO);
+        infoPanel.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            FaceControl(collision.transform.position);
+            infoPanel.SetActive(true);
+            isPlayerNear = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            infoPanel.SetActive(false);
+            isPlayerNear = false;
+        }
+    }
+
+    private void FaceControl(Vector2 targetPos)
+    {
+        if (facingRight == 1 && targetPos.x < transform.position.x || facingRight == -1 && targetPos.x > transform.position.x)
+        {
+            facingRight *= -1;
+            transform.rotation = Quaternion.Euler(0, facingRight == 1 ? 0 : 180, 0);
+            infoPanel.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
